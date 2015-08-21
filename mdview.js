c16d2ebfcb7810e279beae2eac395da8a63eb194
Colin Atkinson <colin.william.atkinson@gmail.com>
2015-08-21 15:49:42 -0400

    Add better markdown parsing support

--- mdview.js
+++ mdview.js
@@ -1,7 +1,19 @@
 #!/bin/env node
 var blessed = require('blessed');
 var fs = require('fs');
-var md = require('markdown').markdown;
+var htmlToText = require('html-to-text');
+var marked = require('marked');
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false
+});
 
 function mainBox(cont) {
     // Create a screen object.
@@ -36,49 +48,13 @@ function mainBox(cont) {
     screen.render();
 }
 
+// Convert the Markdown data to
 function convertMD(data) {
-    var p = md.parse(data);
-    var val = '';
-    for(var i = 1; i < p.length; i++) {
-        var cur = p[i];
-        switch(cur[0]) {
-            case 'header':
-                val += '{blue-fg}' + cur[2] + '{/blue-fg}\n';
-                break;
-            case 'numberlist':
-                for(var j = 1; j < cur.length; j++) {
-                    val += j + '. ' + cur[j][1] + '\n';
-                }
-                break;
-            case 'para':
-                if(cur.length > 2) {
-                    for(var k = 1; k < cur.length; k++) {
-                        if(cur[k].constructor === Array) {
-                            if(cur[k][0] == 'link') {
-                                val += cur[k][2] + ' (' + cur[k][1].href + ')';
-                            }
-                        } else {
-                            val += cur[k];
-                        }
-                    }
-                    break;
-                }
-            default:
-                //arr.push([blessed.box, {tags:false, content: JSON.stringify(p[i][1])}]);
-                // console.log('ey');
-                val += cur[cur.length - 1] + '\n';
-        }
-    }
-    return val;
+    var html = marked(data);
+    var txt = htmlToText.fromString(html, {tables: true});
+    return txt;
 }
 
-// Create some example content
-/*var c = '';
-for(var i = 0; i < 100; i++) {
-    c += 'Test {blue-fg}' + i + '{/blue-fg}\n';
-}*/
-
-var buf = fs.readFileSync('./fantasy2015.md', {encoding: 'utf8'});
+var buf = fs.readFileSync('test_files/fantasy2015.md', {encoding: 'utf8'});
 
 mainBox(convertMD(buf));
-//convertMD(buf);
